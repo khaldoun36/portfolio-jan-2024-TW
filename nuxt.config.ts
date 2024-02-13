a187c0bff616144cf1734dffcddf6d6477c0b672
khaldoun36 <khaldoun36@gmail.com>
2024-02-13 20:50:39 +0400

    followup 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  // global head config
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      title: "Khaldoon.dev",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Looking for a web designer in Dubai? I'm Khaldoon, an independent developer specializing in crafting user-friendly web applications that leave a lasting impression. Let's elevate your online presence together!",
+        },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content:
+            "Dubai web design, Dubai web developer, user experience design, UI/UX design, web application development",
+        },
+        { hid: "author", name: "author", content: "John Doe" },
+      ],
+      link: [
+        {
+          rel: "preload",
+          href: "./public/fonts/GeistVariableVF.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+      ],
+    },
+  },
 
+  // ./global head config
   // my image presets
   image: {
     presets: {
